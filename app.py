9fb96e758c1bcc7f79881f9ce9302464d098cc52
DennisNi08 <cgh7bv@virginia.edu>
2026-06-24 17:06:18 -0400

    Finished search CIK feature

--- app.py
+++ app.py
@@ -8,14 +8,36 @@ class SecEdgar:
 
         headers = {'user-agent': 'MLT dennis7ni@gmail.com'}
         r = requests.get(self.fileurl, headers=headers)
-
-        print(r.text)
-        print(self.filejson)
+        self.filejson = r.json()
+        #print(r.text)
+        #print(self.filejson)
 
         self.clk_json_to_dict()
 
     def clk_json_to_dict(self):
         self.namedict = {}
         self.tickerdict = {}
+        for item in self.filejson.values():
+            cik = str(item['cik_str']).zfill(10)
+            name = item['title']
+            ticker = item['ticker']
 
-se = SecEdgar("https://www.sec.gov/files/company_tickers.json") 
+            self.namedict[name] = cik
+            self.tickerdict[ticker] = cik
+    
+    def print_entry_name(self, name):
+        cik = self.namedict.get(name)
+        ticker = [k for k, v in self.tickerdict.items() if v == cik]
+        print(f"Name: {name}, CIK: {cik}, Ticker: {ticker[0] if ticker else 'N/A'}")
+
+
+
+    def print_entry_ticker(self, ticker):
+        cik = self.tickerdict.get(ticker)
+        name = [k for k, v in self.namedict.items() if v == cik]
+        print(f"Ticker: {ticker}, CIK: {cik}, Name: {name[0] if name else 'N/A'}")
+        
+
+sec = SecEdgar("https://www.sec.gov/files/company_tickers.json") 
+print(sec.print_entry_name("Apple Inc."))     # Example name for Apple Inc.
+print(sec.print_entry_ticker("AAPL"))       # Example ticker for Apple Inc.
